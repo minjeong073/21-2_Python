e865afeee1b4856d36aea95f9404ed82d4919e5e
김민정 <91234288+minjeong073@users.noreply.github.com>
2021-10-15 11:51:44 +0900

    Create BJ_test_result.py

---
+++ BJ_test_result.py
@@ -0,0 +1,18 @@
+score = int(input())
+
+if 89< score <=100 :
+  print("A")
+elif 79< score :
+  print("B")
+elif 69< score :
+  print("C")
+elif 59< score :
+  print("D")
+else :
+  print("F")
+
+  
+/*출력
+score = 90 --> A
+score = 78 --> C
+*/
